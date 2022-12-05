a7b39327ad6c37729f649138197344df7669e5eb
m-kotelnikov <64478845+m-kotelnikov@users.noreply.github.com>
2022-12-05 17:11:11 +0300

    Update swiper.js

--- swiper.js
+++ swiper.js
@@ -1,6 +1,6 @@
 $(".slider-main_component").each(function(index) {
   const swiper = new Swiper($(this).find(".swiper")[0], {
-    slidesPerView: 3,
+    slidesPerView: 2,
     speed: 700,
     keyboard: true,
     // mousewheel: true,
